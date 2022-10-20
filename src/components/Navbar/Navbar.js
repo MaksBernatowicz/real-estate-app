c149fe62173bf7907bad7a7ed09888022afa53c3
twój login do GitHuba <96842769+MaksBernatowicz@users.noreply.github.com>
2022-10-20 16:09:50 +0200

    added useState for hamburger menu with hoover

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import { HiOutlineMenuAlt4 } from 'react-icons/hi'
-import { FaRegTimesCircle } from 'react-icons/fa'
+import React, { useState } from 'react'
+import { HiMenu } from 'react-icons/hi'
 import { BsFillHouseFill } from 'react-icons/bs'
+import { MdClose } from 'react-icons/md'
 
 import './Navbar.css'
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
     return (
         <div className='navbar'>
             <div className='container'>
                 <h1><span><BsFillHouseFill />Real</span>Estate</h1>
                 <button className='btn'>Sign In</button>
-                <ul className='nav-menu'>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                     <li><a href='#'>Home</a></li>
                     <li><a href='#'>Search</a></li>
                     <li><a href='#'>About</a></li>
                     <li><a href='#'>Contact</a></li>
                 </ul>
-                <div className='hamburger'>
-                    <HiOutlineMenuAlt4 className='icon' />
+                <div className='hamburger' onClick={handleClick}>
+                    {click ? (<MdClose className='icon' />) : (<HiMenu className='icon' />)}
                 </div>
             </div>
         </div>
