08e5dc8cc055f9269667b12a2c3fb23f27aa1d8e
twój login do GitHuba <96842769+MaksBernatowicz@users.noreply.github.com>
2022-10-20 18:10:41 +0200

    added body for Best.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Best from "./components/best/Best";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Best />
     </div>
   );
 }
