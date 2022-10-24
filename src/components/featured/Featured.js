a52f166351b44e11514cc77835e04692513ae215
twój login do GitHuba <96842769+MaksBernatowicz@users.noreply.github.com>
2022-10-24 18:04:37 +0200

    added hover effect in navbar

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -6,13 +6,13 @@ import House1 from '../../assets/house 1/house1.jpg'
 import Bed1 from '../../assets/house 1/bed1.jpg'
 import Livingroom1 from '../../assets/house 1/livingroom1.jpg'
 import Kitchen1 from '../../assets/house 1/kitchen1.jpg'
-import Bathroom from '../../assets/bath1.jpg'
+import Bath1 from '../../assets/house 1/bath1.jpg'
 
 import House2 from '../../assets/house 2/house2.jpg'
-import Bed3 from '../../assets/bed3.jpg'
-import Bed4 from '../../assets/bed4.jpg'
-import Livingroom from '../../assets/living-room.jpg'
-import Bathroom2 from '../../assets/bath2.jpg'
+import Bed2 from '../../assets/house 2/bed2.jpg'
+import Livingroom2 from '../../assets/house 2/livingroom2.jpg'
+import Kitchen2 from '../../assets/house 2/kitchen2.jpg'
+import Bath2 from '../../assets/house 2/bath2.jpg'
 
 const Featured = () => {
     return (
@@ -26,8 +26,8 @@ const Featured = () => {
                 <img className='span-3 image-grid-row-2' src={House1} alt='House 1' />
                 <img src={Bed1} alt='Bedroom 1' />
                 <img src={Livingroom1} alt='Livingroom 1' />
-                <img src={Kitchen1} alt='Kitchen' />
-                <img src={Bathroom} alt='Bathroom 1' />
+                <img src={Kitchen1} alt='Kitchen 1' />
+                <img src={Bath1} alt='Bathroom 1' />
                 <div className='span-3 img-details'>
                     <div className='top'>
                         <h2>123 Acme St. Dallas, TX</h2>
@@ -62,13 +62,13 @@ const Featured = () => {
             {/* --------------------\\\\\  SECOND SECTION  /////-------------------- */}
 
             <div className='container'>
-                <img className='order-2' src={Bed3} alt='Bedroom 3' />
-                <img className='order-3' src={Bed4} alt='Bedroom 4' />
+                <img className='order-2' src={Bed2} alt='Bedroom 2' />
+                <img className='order-3' src={Livingroom2} alt='Livingroom 2' />
 
                 <img className='span-3 image-grid-row-2 order-1' src={House2} alt='House 2' />
 
-                <img className='order-4' src={Bathroom2} alt='Bathroom 2' />
-                <img className='order-5' src={Livingroom} alt='Livingroom' />
+                <img className='order-4' src={Bath2} alt='Bathroom 2' />
+                <img className='order-5' src={Kitchen2} alt='Livingroom' />
                 <div className='span-2 right-img-details order-7'>
                     <p>A beautiful modern day home in the city with a full-size pool. Spacious and luxurious home located in Texas. Including media room, workout facility, and built-in sauna.</p>
                     <button className='btn'>View Listing</button>
