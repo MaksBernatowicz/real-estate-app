04c5fa7fa040cf14e684d8f0da6addf553043626
twój login do GitHuba <96842769+MaksBernatowicz@users.noreply.github.com>
2022-10-24 16:13:47 +0200

    changed hero photo, added var() colours

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -7,8 +7,8 @@ const Hero = () => {
     return (
         <div className='hero'>
             <div className='content'>
-                <h1>Find the perfect place</h1>
-                <p className='search-text'> Search the largest selection of luxury high-rise apartments, multi-family homes and luxury homes.</p>
+                <h1>Find your new home</h1>
+                <p className='search-text'> Search our great selection of family homes, luxurious mansions and apartments.</p>
                 <form className='search'>
                     <div>
                         <input type='text' placeholder='Enter Keyword..' />
